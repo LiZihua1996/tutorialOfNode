67e6b323281b6d10524a0ccb20bfbb33805772d6
LiZihua1996 <LiZihua1996@github.com>
2020-03-20 18:30:41 +0800

    hello

--- hello.js
+++ hello.js
@@ -1,15 +1,15 @@
 const http = require("http")
-const childProcess = require("child_process")
+const childProcess = require("child_process")   //内置模块,用于创建子进程
 const host = "127.0.0.1"
 const port = 7777
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {    //响应请求
     res.statusCode = 200
     res.setHeader("Content-Type", "text/plain")
     res.end("Hello, Node")
 })
 
-server.listen(port, hostname, ()=>{
+server.listen(port, hostname, ()=>{ 监听端口,并设置回调函数
     console.log(`Server running at http://${host}:${port}`)
-    childProcess.exec(`start http://${hostname}:${port}/`)
+    childProcess.exec(`start http://${hostname}:${port}/`)  //在浏览器中打开
 })
